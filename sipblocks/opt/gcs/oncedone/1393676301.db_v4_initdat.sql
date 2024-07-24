78c0409dcb95cf84060448c6bfdc5fae6ac807b9
Jeffrey Stokoe <admin@aelintra.com>
2024-07-23 23:06:24 -0400

    Delete sipblocks/opt/gcs/oncedone/1393676301.db_v4_initdat.sql

--- sipblocks/opt/gcs/oncedone/1393676301.db_v4_initdat.sql
+++
@@ -1,7 +0,0 @@
-BEGIN TRANSACTION;
-
-INSERT OR IGNORE INTO Route(pkey,active,auth,cluster,description,dialplan,path1,path2,path3,path4) values ('DEFAULT','YES','NO','default','DEFAULT TRUNK','_XXXX.','None','None','None','None');
-INSERT OR IGNORE INTO lineIO(pkey,active,carrier,closeroute,cluster,description,moh,openroute,peername,routeclassopen,routeclassclosed,swoclip,technology) values ('_XXXX.','YES','PTT_DiD_Class','Operator','default','user1804','NO','Operator','peer1798','100','100','NO','Class');
-INSERT OR IGNORE INTO speed(pkey,cluster,devicerec,grouptype,outcome,outcomerouteclass,ringdelay) values ('RINGALL','default','default','Ring','Operator','100','180');
-
-COMMIT;
